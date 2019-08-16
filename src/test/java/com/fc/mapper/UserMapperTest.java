54751a58e759cd22b9f5bedc9855d4c5d73fd14c
lidongpeng <lidongpeng@autohome.com.cn>
2019-08-16 15:27:50 +0800

    邮件服务器改为163服务器

--- src/test/java/com/fc/mapper/UserMapperTest.java
+++ src/test/java/com/fc/mapper/UserMapperTest.java
@@ -1,11 +1,17 @@
 package com.fc.mapper;
 
+import com.fc.util.MyConstant;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import redis.clients.jedis.Jedis;
+
+import javax.mail.internet.MimeMessage;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:application-context.xml","classpath:spring-mvc.xml"})
@@ -19,5 +25,26 @@ public class UserMapperTest {
         for (int i = 0; i < 50; i++) {
             Long b =jedis.del("post_prefix"+i);
         }
+    }
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Test
+    public void testMail(){
+        javaMailSender.send(new MimeMessagePreparator(){
+
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                System.out.println("开始发邮件...");
+                MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,true,"UTF-8");
+                mimeMessageHelper.setFrom("13153771737@163.com");
+                mimeMessageHelper.setTo("821888642@qq.com");
+                mimeMessageHelper.setSubject("一封激活邮件");
+                StringBuilder sb  = new StringBuilder();
+                sb.append("<html><head></head><body>");
+                mimeMessageHelper.setText(sb.toString(),true);
+
+                System.out.println("结束发邮件...");
+            }
+        });
     }
 }
